49f9a7b74cb51d928a3d7d409bd8f87c6f5ec85e
Luis Arpasi <meclipse@gmail.com>
2018-12-16 21:05:38 -0500

    Update location.js

--- api/location.js
+++ api/location.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 const googleApiUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address='
-const googleApiKey = 'AIzaSyA5mjCwx1TRLuBAjwQw84WE6h5ErSe7Uj8'
+const googleApiKey = //Google Api Key
 
 const openWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?'
-const openWeatherApiKey = 'f369635965b00ad16ced5da4da4b9f3b'
+const openWeatherApiKey = //Open Weather ApiKey
 
 export const GetCoords = async (address) => {
     let encodedAddress = encodeURI(address)
